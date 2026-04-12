6878f297fc3af35f16b2cecbd4d525d7214225b3
srinagasrivineeth <srinagasrivineeth@gmail.com>
2026-04-12 17:18:42 +0530

    adding url

--- EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200", "http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200", "http://localhost:4200", "https://employee-ui-b3dq.onrender.com/")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
@@ -135,7 +135,7 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 app.UseRouting();
 // 🔥 USE CORS HERE (VERY IMPORTANT POSITION)
-app.UseCors("AllowAll");
+app.UseCors("AllowFrontend");
 app.Use(async (context, next) =>
 {
     if (context.Request.Method == "OPTIONS")
